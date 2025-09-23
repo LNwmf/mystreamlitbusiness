4ca73eb528986268dea691b5ef240093948dec60
LNwmf <lisly@theworldmusicfoundation.org>
2025-09-23 16:49:00 -0500

    color

--- Home.py
+++ Home.py
@@ -6,20 +6,7 @@ st.image("https://theworldmusicfoundation.org/wp-content/uploads/2016/11/wmf_sma
 st.title("About Us")
 st.write("This app is created by the World Music Foundation to share music and uplift local businesses.")
 
-st.markdown(
-        """
-        <style>
-        .streamlit-expanderHeader p {
-            font-size: 50px;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
 
-with st.expander('Our goals', expanded=True):
-  st.write('The World Music Foundation is a 501(c)(3) non-profit organization dedicated to a simple mission: opening minds through Music. We believe, and research shows, that experiencing music of other cultures increases tolerance and cultural empathy in people, and everything that we do is based on effecting this social change. Racism continues to be one of the world’s greatest problems and Music is a proven armament in this fight.')
-
-#st.markdown("---")
-#with st.expander("Learn more about us and our goals"):
-#    st.write("https://theworldmusicfoundation.org/")
+st.markdown("---")
+with st.expander("Our purpose"):
+    st.write("The World Music Foundation is a 501(c)(3) non-profit organization dedicated to a simple mission: opening minds through Music. We believe, and research shows, that experiencing music of other cultures increases tolerance and cultural empathy in people, and everything that we do is based on effecting this social change. Racism continues to be one of the world’s greatest problems and Music is a proven armament in this fight.")

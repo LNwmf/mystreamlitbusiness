4ddb04ac2fde671f1f44ef34b4b92fd4a3e0ebab
LNwmf <lisly@theworldmusicfoundation.org>
2025-09-23 17:13:49 -0500

    color

--- Home.py
+++ Home.py
@@ -8,11 +8,11 @@ st.write("This app is created by the World Music Foundation to share music and u
 
 
 st.markdown("---")
-with st.expander("Our Purpose"):
+with st.expander("Our Purpose", icon=":material/target:"):
     st.write("The World Music Foundation is a 501(c)(3) non-profit organization dedicated to a simple mission: opening minds through Music. We believe, and research shows, that experiencing music of other cultures increases tolerance and cultural empathy in people, and everything that we do is based on effecting this social change. Racism continues to be one of the world’s greatest problems and Music is a proven armament in this fight.")
 
 #st.markdown("")
-with st.expander("Contact Us", icon=":material/target:"):
+with st.expander("Contact Us", icon=":material/emoji_people:"):
     st.write("Inquires: info@theworldmusicfoundation.org")
     st.write()
     st.write("Events: events@theworldmusicfoundation.org")

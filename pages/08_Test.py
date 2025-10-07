c1d3742bba3832e948ac3f0b3b89b7be7f63c4d9
LNwmf <lisly@theworldmusicfoundation.org>
2025-10-07 16:23:35 -0500

    sheets

--- pages/08_Test.py
+++ pages/08_Test.py
@@ -8,8 +8,8 @@ selected_option = st.radio(
 )
 
 if selected_option == "Image A":
-    st.image("image1.png", caption="This is Image A")
+    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJvbABjR5D8Ja6B51Y55dbqqL0VMW85XdV6w&s", caption="This is Image A")
 elif selected_option == "Image B":
-    st.image("image2.png", caption="This is Image B")
+    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwBNHTvWu7XJyqeE2yDf3n4ezQAHThGfxjeQ&s", caption="This is Image B")
 elif selected_option == "Image C":
-    st.image("image3.png", caption="This is Image C")
+    st.image("https://sylviawakana.com/wp-content/uploads/2022/07/Taiyaki-1.jpg", caption="This is Image C")

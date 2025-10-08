5c38a72218d1027c11662da69526453c06804264
LNwmf <lisly@theworldmusicfoundation.org>
2025-10-07 19:05:35 -0500

    sheets

--- pages/08_Test.py
+++ pages/08_Test.py
@@ -1,19 +1,19 @@
 import streamlit as st
 from st_clickable_images import clickable_images
 
-st.title("Image Display based on Radio Selection")
+#st.title("Image Display based on Radio Selection")
 
-selected_option = st.radio(
-    "Choose an image:",
-    options=["Image A", "Image B", "Image C"]
-)
+#selected_option = st.radio(
+#    "Choose an image:",
+#    options=["Image A", "Image B", "Image C"]
+#)
 
-if selected_option == "Image A":
-    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJvbABjR5D8Ja6B51Y55dbqqL0VMW85XdV6w&s", caption="This is Image A")
-elif selected_option == "Image B":
-    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwBNHTvWu7XJyqeE2yDf3n4ezQAHThGfxjeQ&s", caption="This is Image B")
-elif selected_option == "Image C":
-    st.image("https://sylviawakana.com/wp-content/uploads/2022/07/Taiyaki-1.jpg", caption="This is Image C")
+#if selected_option == "Image A":
+#    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJvbABjR5D8Ja6B51Y55dbqqL0VMW85XdV6w&s", caption="This is Image A")
+#elif selected_option == "Image B":
+#    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwBNHTvWu7XJyqeE2yDf3n4ezQAHThGfxjeQ&s", caption="This is Image B")
+#elif selected_option == "Image C":
+#    st.image("https://sylviawakana.com/wp-content/uploads/2022/07/Taiyaki-1.jpg", caption="This is Image C")
 
 #clickable images
 st.write("Pick a food:")

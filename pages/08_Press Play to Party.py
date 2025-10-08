913627984fd4fe3193e272678db4bc9c891797eb
LNwmf <lisly@theworldmusicfoundation.org>
2025-10-07 19:11:12 -0500

    sheets

--- pages/08_Press Play to Party.py
+++ pages/08_Press Play to Party.py
@@ -12,20 +12,8 @@ st.title("🎉 Press Play to Party")
 st.markdown("""
 Welcome! Pick an energy below to discover a blended playlist, support a local BIPOC business, and unlock a reward for sharing with others.
 """)
-#st.title("Image Display based on Radio Selection")
-
-#selected_option = st.radio(
-#    "Choose an image:",
-#    options=["Image A", "Image B", "Image C"]
-#)
-
-#if selected_option == "Image A":
-#    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJvbABjR5D8Ja6B51Y55dbqqL0VMW85XdV6w&s", caption="This is Image A")
-#elif selected_option == "Image B":
-#    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwBNHTvWu7XJyqeE2yDf3n4ezQAHThGfxjeQ&s", caption="This is Image B")
-#elif selected_option == "Image C":
-#    st.image("https://sylviawakana.com/wp-content/uploads/2022/07/Taiyaki-1.jpg", caption="This is Image C")
-
+st.write("")
+st.write("")
 #clickable images
 st.write("Pick a food:")
 images = [
@@ -50,3 +38,20 @@ if clicked > -1:
     st.markdown(f"**{titles[clicked]}** selected")
 else:
     st.markdown("No image clicked")
+
+
+#st.title("Image Display based on Radio Selection")
+
+#selected_option = st.radio(
+#    "Choose an image:",
+#    options=["Image A", "Image B", "Image C"]
+#)
+
+#if selected_option == "Image A":
+#    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJvbABjR5D8Ja6B51Y55dbqqL0VMW85XdV6w&s", caption="This is Image A")
+#elif selected_option == "Image B":
+#    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwBNHTvWu7XJyqeE2yDf3n4ezQAHThGfxjeQ&s", caption="This is Image B")
+#elif selected_option == "Image C":
+#    st.image("https://sylviawakana.com/wp-content/uploads/2022/07/Taiyaki-1.jpg", caption="This is Image C")
+
+#clickable images

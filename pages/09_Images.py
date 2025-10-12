182bf747c39e27496490f53fbe79501b69f30341
LNwmf <lisly@theworldmusicfoundation.org>
2025-10-12 14:54:09 -0500

    images

--- pages/09_Images.py
+++ pages/09_Images.py
@@ -6,8 +6,4 @@ st.set_page_config(
     layout="centered",
 )
 
-st.image("https://drive.google.com/uc?export=view&id=1vHzvfjkCdA2WSqzwv-qD3fpzZq3wCIOY", width=300)
-st.title("🪞 Serene Self-Care")
-st.markdown("""
-Welcome! Pick an energy below to discover a blended playlist, support a local BIPOC business, and unlock a reward for sharing with others.
-""")
+st.image("https://drive.google.com/uc?export=view&id=1vHzvfjkCdA2WSqzwv-qD3fpzZq3wCIOY")

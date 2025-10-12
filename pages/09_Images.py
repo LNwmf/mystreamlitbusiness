85ef1eb7695f2081943a8d1d2630b78f026fed34
LNwmf <lisly@theworldmusicfoundation.org>
2025-10-12 14:53:01 -0500

    sheets

---
+++ pages/09_Images.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Serene Self-Care",
+    page_icon="🪞",
+    layout="centered",
+)
+
+st.image("https://drive.google.com/uc?export=view&id=1vHzvfjkCdA2WSqzwv-qD3fpzZq3wCIOY", width=300)
+st.title("🪞 Serene Self-Care")
+st.markdown("""
+Welcome! Pick an energy below to discover a blended playlist, support a local BIPOC business, and unlock a reward for sharing with others.
+""")

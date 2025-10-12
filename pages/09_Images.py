3c9f503cab2ac84ef6ddd8eac3a30b1b147f144e
LNwmf <lisly@theworldmusicfoundation.org>
2025-10-12 15:06:47 -0500

    images

--- pages/09_Images.py
+++ pages/09_Images.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
-image_url = "https://drive.google.com/uc?export=view&id=15faTIUuXxjIVB8vbJ55zdYh1qvMqHrM_"
-st.image(image_url, caption="Image from Google Drive")
+st.image("https://drive.google.com/uc?export=view&id=1Zahx9e0ogGncVZdoPyBZJeqY1TJ6-Q_g")
 

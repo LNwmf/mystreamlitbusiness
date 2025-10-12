c835be23f8bfc6bb456b7151394032aca47a65a9
LNwmf <lisly@theworldmusicfoundation.org>
2025-10-12 15:27:50 -0500

    images

--- pages/09_Images.py
+++ pages/09_Images.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
-image_url = "https://i.imgur.com/3ACcDlF.jpeg"
-st.image(image_url, caption="Birria tacos", width=200)
+birria = "https://i.imgur.com/3ACcDlF.jpeg"
+st.image(birria, caption="Birria tacos", width=200)
 
+pasta = "https://i.imgur.com/8wTBiHb.jpeg"
+st.image(pasta, caption="Pasta", width=200)
+
+burger = "https://i.imgur.com/y0Ezg5M.jpeg"
+st.image(burger, caption="Burger", width=200)
+
+ice_cream = "https://i.imgur.com/QDmOxZ8.jpeg"
+st.image(ice_cream, caption="Ice cream", width=200)
+
+taiyaki = "https://i.imgur.com/CNre03d.jpeg"
+st.image(taiyaki, caption="Taiyaki", width=200)

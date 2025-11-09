75e25826e1630b766b249b01df7a739788632283
LNwmf <lisly@theworldmusicfoundation.org>
2025-11-09 14:56:37 -0600

    images

---
+++ pages/09_Guess the Song.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Sweater Weather Tunes",
+    page_icon="🍂",
+    layout="centered",
+)
+
+st.image("https://theworldmusicfoundation.org/wp-content/uploads/2016/11/wmf_small_logo.png.webp", width=180)
+st.title("🍂 Sweater Weather Tunes")
+st.markdown("""
+Welcome! Pick a fall activity below to discover a blended playlist, support a local BIPOC business, and unlock a reward for sharing with others.
+""")

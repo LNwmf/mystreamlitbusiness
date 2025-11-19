fffaee022f10c1d0b24b1cb44233ec88cbfc32bf
LNwmf <lisly@theworldmusicfoundation.org>
2025-11-18 19:55:21 -0600

    images

--- pages/12_Navigation.py
+++ pages/12_Navigation.py
@@ -1,8 +1,20 @@
 import streamlit as st
 
+st.title("🏠 Music Hub")
+st.markdown("Welcome! Select a page to explore:")
 
+# List of pages
+pages = [
+    "Jams & Juice",
+    "Sweet Melodies",
+    "Y2K Rewind",
+    "Sweater Weather Tunes",
+    "Craft & Sip",
+    "Serene Self-Care",
+    "Press Play to Party",
+    "Guess the Instrument!",
+    "Music Trivia"
+]
 
-pages = st.get_pages()  # Get all pages
-st.write("Pages detected by Streamlit:")
-st.write(pages)
-
+for page in pages:
+    st.page_link(page, label=page)

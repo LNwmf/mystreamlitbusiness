94835f3bee1fd3a892847470cd2e5718e5edb772
LNwmf <lisly@theworldmusicfoundation.org>
2025-11-19 07:17:14 -0600

    images

--- pages/12_Navigation.py
+++ pages/12_Navigation.py
@@ -1,15 +1,22 @@
-import os
 import streamlit as st
 
-st.title("Check Page Filenames")
+pages = {
+    "Jams & Juice": "02_Jams_and_Juice",
+    "Sweet Melodies": "03_Sweet_Melodies",
+    "Y2K Rewind": "04_Y2K_Rewind",
+    "Sweater Weather Tunes": "05_Sweater_Weather_Tunes",
+    "Craft & Sip": "06_Craft_and_Sip",
+    "Serene Self-Care": "07_Serene_Self_Care",
+    "Press Play to Party": "08_Press_Play_to_Party",
+    "Guess the Instrument": "09_Guess_the_Instrument",
+    "Music Trivia": "10_Music_Trivia",
+    "Images": "11_Images",
+}
 
-pages_folder = "pages"
+choice = st.selectbox("Choose a page:", pages.keys())
 
-if os.path.exists(pages_folder):
-    st.write("Files found in `pages/` folder:")
-    for file in os.listdir(pages_folder):
-        st.write("•", file)
-else:
-    st.error("No `pages` folder found.")
+if st.button("Go"):
+    st.switch_page(f"pages/{pages[choice]}.py")
+
 
 

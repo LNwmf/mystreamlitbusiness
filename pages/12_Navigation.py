786403cc046c7aebb9dfcfa658b11793a8499e40
LNwmf <lisly@theworldmusicfoundation.org>
2025-11-18 19:53:20 -0600

    images

--- pages/12_Navigation.py
+++ pages/12_Navigation.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.write(st.get_pages())
+st.title("Check Streamlit Pages")
+
+pages = st.get_pages()  # Get all pages
+st.write("Pages detected by Streamlit:")
+st.write(pages)
+

744cb4a8ad4876d4604ca7999a7969a14d2fe0b2
LNwmf <lisly@theworldmusicfoundation.org>
2025-11-18 19:51:32 -0600

    images

--- pages/12_Navigation.py
+++ pages/12_Navigation.py
@@ -4,20 +4,18 @@ st.title("🏠 Music Hub")
 st.markdown("Welcome! Select a page to explore:")
 
 # List of pages
-pages = {
-    "Jams & Juice": "jams_and_juice",
-    "Sweet Melodies": "sweet_melodies",
-    "Y2K Rewind": "y2k_rewind",
-    "Sweater Weather Tunes": "sweater_weather_tunes",
-    "Craft & Sip": "craft_and_sip",
-    "Serene Self-Care": "serene_self_care",
-    "Press Play to Party": "press_play_to_party",
-    "Guess the Instrument!": "guess_the_instrument",
-    "Music Trivia": "music_trivia"
-}
+pages = [
+    "Jams & Juice",
+    "Sweet Melodies",
+    "Y2K Rewind",
+    "Sweater Weather Tunes",
+    "Craft & Sip",
+    "Serene Self-Care",
+    "Press Play to Party",
+    "Guess the Instrument!",
+    "Music Trivia"
+]
 
-for page_name, page_key in pages.items():
-    if st.button(page_name):
-        # Set query param to redirect
-        st.experimental_set_query_params(page=page_key)
-        st.rerun()
+for page in pages:
+    st.page_link(page, label=page)
+

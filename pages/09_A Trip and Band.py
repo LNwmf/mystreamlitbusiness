e8eb4d4e978a249971517a66462a0e4843298ef7
LNwmf <lisly@theworldmusicfoundation.org>
2025-11-26 08:47:18 -0600

    images

--- pages/09_A Trip and Band.py
+++ pages/09_A Trip and Band.py
@@ -1,1 +1,24 @@
 import streamlit as st
+
+st.set_page_config(
+    page_title="Plan a Trip!",
+    page_icon="✈️",
+    layout="centered",
+)
+
+st.markdown(
+    """
+    <style>
+    img {
+        border-radius: 0 !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.image("https://theworldmusicfoundation.org/wp-content/uploads/2016/11/wmf_small_logo.png.webp", width=180)
+
+st.title("✈️ A Trip and Band")
+st.markdown("""
+Welcome! You're preparing for a dream vacation in the summer. Based on your preferences, you'll be matched with a traditional band around the world!
+""")

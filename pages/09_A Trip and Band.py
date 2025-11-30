857e51a8f8b50f58d8763f683c03ce25ea9e4e15
LNwmf <lisly@theworldmusicfoundation.org>
2025-11-30 15:08:27 -0600

    images

--- pages/09_A Trip and Band.py
+++ pages/09_A Trip and Band.py
@@ -48,7 +48,7 @@ images = [
         "https://theworldmusicfoundation.org/wp-content/streamlitimages/doublemeals.jpg",
 ]
 
-titles=["Ulster Fry", "Chipsi Mayai", "Tabbouleh", "Chilaquiles", "Hiyayakko", "Doubles"] #ireland, tanzania, middle east, mexico, japan, caribbean
+titles=["1", "2", "3", "4", "5", "6"] #ireland-Ulster Fry, tanzania-Chipsi Mayai, middle east-Tabbouleh, mexico-Chilaquiles, japan-Hiyayakko, caribbean-Doubles
 
 if "selected_meal" not in st.session_state:
     st.session_state.selected_meal = None

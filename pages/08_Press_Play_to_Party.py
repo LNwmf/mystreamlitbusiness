f0e1a7a0ba7639174cdaca93fe6ea605253208e5
LNwmf <lisly@theworldmusicfoundation.org>
2025-12-09 16:44:04 -0600

    images

--- pages/08_Press_Play_to_Party.py
+++ pages/08_Press_Play_to_Party.py
@@ -258,6 +258,9 @@ if selected_food and selected_genre and selected_theme and selected_complete and
     st.write(f"🌐 [Visit Website]({info['website']})")
     st.write(f"🎁 **Special Offer:** {info['offer']}")
 
+    st.write("")
+    st.subheader("Share your playlist!", divider="grey")
+    st.write("")
     st.write("👥 How many people did you share your playlist with?")
     shared_count = st.number_input("Number of people:", min_value=0, step=1)
 
